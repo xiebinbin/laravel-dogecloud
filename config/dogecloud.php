90eed374f2b46ab2f8718ba8a7640bc2f07726c1
soda <soda@gmail.com>
2023-02-08 16:35:13 +0800

    first commit

---
+++ config/dogecloud.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'enable' => true,
+    'access_key' => env('DOGE_ACCESS_KEY_ID', ''),
+    'secret_key' => env('DOGE_SECRET_ACCESS_KEY', ''),
+    'region' => 'automatic',
+    'bucket' => env('DOGE_BUCKET'),
+    'url' => env('DOGE_URL'),
+    'endpoint' => env('DOGE_ENDPOINT'),
+    'use_path_style_endpoint' => env('DOGE_USE_PATH_STYLE_ENDPOINT', false),
+    'throw' => false,
+];
---
+++ src/DogeCloud.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Soda\LaravelDogeCloud;
+use Illuminate\Support\Facades\Cache;
+
+class DogeCloud
+{
+    /**
+     * @throws \Exception
+     */
+    public static function refreshDogeCloudToken(): void
+    {
+        $token = Cache::get('dogecloud.token');
+        if (empty($token)) {
+            $api = self::dogeCloudApi('/auth/tmp_token.json', array(
+                "channel" => "OSS_FULL",
+                "scopes" => array("*")
+            ), true);
+            if ($api && $api['code'] == 200) {
+                $credentials = $api['data']['Credentials'];
+                Cache::put('dogecloud.token', $credentials['sessionToken'], now()->addHours(2));
+                Cache::put('dogecloud.access_key_id', $credentials['accessKeyId'], now()->addHours(2));
+                Cache::put('dogecloud.secret_access_key', $credentials['secretAccessKey'], now()->addHours(2));
+            } else {
+                // 失败
+                throw new \Exception('doge token error');
+            }
+        }
+    }
+    public static function dogeCloudApi($apiPath, $data = array(), $jsonMode = false)
+    {
+        $accessKey = config('dogecloud.key');
+        $secretKey = config('dogecloud.secret');
+
+        $body = $jsonMode ? json_encode($data) : http_build_query($data);
+        $signStr = $apiPath . "\n" . $body;
+        $sign = hash_hmac('sha1', $signStr, $secretKey);
+        $Authorization = "TOKEN " . $accessKey . ":" . $sign;
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://api.dogecloud.com" . $apiPath);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+        if (isset($data) && $data) {
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: ' . ($jsonMode ? 'application/json' : 'application/x-www-form-urlencoded'),
+                'Authorization: ' . $Authorization
+            ));
+        }
+
+        $ret = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($ret, true);
+    }
+}
